bb977e94d594a48181f950e56084712de8e5cc63
Oleg Abdrashitov <olegabu@users.noreply.github.com>
2018-08-04 16:36:47 -0400

    Create package.js

---
+++ package.js
@@ -0,0 +1,24 @@
+{
+	"name": "chaincode-node-storage",
+	"version": "1.0.0",
+	"description": "base class for chaincodes in node.js",
+	"engines": {
+		"node": ">=8.4.0",
+		"npm": ">=5.3.0"
+	},
+    "repository": {
+        "type": "git",
+        "url": "git+https://github.com/olegabu/chaincode-node-storage.git"
+    },
+    "keywords": ["chaincode","hyperledger"],
+    "author": "Oleg Abdrashitov <https://github.com/olegabu>",
+    "license": "Apache-2.0",
+    "bugs": {
+        "url": "https://github.com/olegabu/chaincode-node-storage/issues"
+    },
+    "homepage": "https://github.com/olegabu/chaincode-node-storage#readme",
+	"engine-strict": true,
+	"dependencies": {
+		"fabric-shim": "~1.1.0"
+	}
+}
